9f117f3d002634ffb236cd2af2d3f10667ddc680
devahadul <abc.ahadul@gmail.com>
2024-07-03 17:26:26 +0600

    added navbar

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,6 +1,7 @@
 import { NavLink } from 'react-router-dom';
 import Logo from '../../assets/logo dark.png';
 import { MdArrowOutward } from "react-icons/md";
+import { useRef, useState } from 'react';
 const Navbar = () => {
     const links =
         <>
@@ -37,16 +38,26 @@ const Navbar = () => {
                     About Us</NavLink>
             </li>
         </>
+    const [dropDownState, setDropDownState] = useState(false);
+    const dropDownMenuRef = useRef();
     return (
         <div className="my-2 md:my-3 lg:my-6">
-            <nav className='flex items-center justify-between'>
+            <nav className="flex items-center justify-between px-4 py-2 mb-24">
                 <div className='cursor-pointer'>
                     <img src={Logo} alt="" />
                 </div>
-                <ul className='flex items-center justify-between gap-6'>
+                <ul className="hidden items-center justify-between gap-10 md:flex">
                     {links}
                 </ul>
-                <div className='text-primary'>
+                <div ref={dropDownMenuRef} onClick={() => setDropDownState(!dropDownState)} className="relative flex transition-transform md:hidden">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="cursor-pointer" > <line x1="4" x2="20" y1="12" y2="12" /> <line x1="4" x2="20" y1="6" y2="6" /><line x1="4" x2="20" y1="18" y2="18" /> </svg>
+                    {dropDownState && (
+                        <ul className="  z-10  gap-2 absolute right-0 top-11 flex w-[200px] flex-col justify-center items-center rounded-lg text-base bg-gray-100 p2">
+                            {links}
+                        </ul>
+                    )}
+                </div>
+                <div className='text-primary hidden lg:block'>
                     <button className='flex gap-2 items-center px-5 py-2 border rounded-lg border-primary font-medium hover:bg-[#FFC637] duration-300'>Appointment <MdArrowOutward size={18} /></button>
                 </div>
             </nav>
